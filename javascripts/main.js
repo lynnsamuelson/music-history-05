3aa3d696dcf873c2e438f17690ad3f3b2c007cf4
Lynn Samuelson <lynn.samuelson@gmail.com>
2015-08-10 13:03:30 -0500

    filter button working

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,81 +21,71 @@ requirejs(
   ["jquery", "lodash", "firebase", "hbs", "bootstrap", "dom-access", "populate-songs", "get-more-songs"], 
   function($, _, _firebase, Handlebars, bootstrap, dom, populate, getMore) {
     
+  var allSongsArray = [];
+
+
+  var myFirebaseRef = new Firebase("https://luminous-fire-170.firebaseio.com/");
+  myFirebaseRef.child("songs").on("value", function(snapshot) {
+    var songs = snapshot.val();
+    console.log(songs);
+
+    require(['hbs!../templates/songs'], function(songTemplate) {
+    $("#songList").append(songTemplate({songs:songs}));
+      //var el = $("#songList");
+    });
     
-    var myFirebaseRef = new Firebase("https://luminous-fire-170.firebaseio.com/");
-    myFirebaseRef.child("songs").on("value", function(snapshot) {
-      var songs = snapshot.val();
-      console.log(songs);
-
-      require(['hbs!../templates/songs'], function(songTemplate) {
-      $("#songList").append(songTemplate({songs:songs}));
-        //var el = $("#songList");
-      });
-      
-      require(['hbs!../templates/artist'], function(artistTemplate) {
-          $("#artistdrpdwn").append(artistTemplate({songs:songs}));
-
-      });
-      require(['hbs!../templates/album'], function(albumTemplate) {
-          $("#albumdrpdwn").append(albumTemplate({songs:songs}));
-     
-      });
-
-      $(document).on("click", "#delbtn", function() {
-          
-          console.log("delete clicked");
-          $(this).parent("div").remove();
-      });
-
+    require(['hbs!../templates/artist'], function(artistTemplate) {
+        $("#artistdrpdwn").append(artistTemplate({songs:songs}));
 
     });
-  
+    require(['hbs!../templates/album'], function(albumTemplate) {
+        $("#albumdrpdwn").append(albumTemplate({songs:songs}));
+   
+    });
 
-    
+    // Convert Firebase's object of objects into an array of objects
+    for (var key in songs) {
+      allSongsArray[allSongsArray.length] = songs[key];
+    }
+      console.log("all songs array", allSongsArray);
 
-    //var counter = 0;
-    // function pageDsp(songs) {
-    //   var currentSong;
-    //   if (counter > 1) {
-    //     //console.log("HERE'S THE IF")
-    //     return;
-    //   } else {
-    //     //console.log("HERE'S THE ELSE!")
-      
-    //     // require(['hbs!../templates/songs'], function(songTemplate) {
-    //     // $("#songList").append(songTemplate(songs));
-    //     //   //var el = $("#songList");
-    //     // });
+    $(document).on("click", "#delbtn", function() {
         
-    //     // require(['hbs!../templates/artist'], function(artistTemplate) {
-    //     //     $("#artistdrpdwn").append(artistTemplate(songs));
-
-    //     // });
-    //     // require(['hbs!../templates/album'], function(albumTemplate) {
-    //     //     $("#albumdrpdwn").append(albumTemplate(songs));
-       
-    //     // });
-
-    //   }
+      console.log("delete clicked");
+      $(this).parent("div").remove();
+    });
 
 
-    //   // $(document).on("click", "#delbtn", function() {
-          
-    //   //     //console.log("delete clicked");
-    //   //     $(this).parent("div").remove();
-    //     //});
+    $('#filterbtn').click(function() {
+        console.log("filter clicked");
 
-    // }
+        var currentArtist = $('#artistdrpdwn').val(); 
+        console.log(currentArtist);
+        var currentAlbum = $('#albumdrpdwn').val(); 
+        console.log(currentAlbum);
+        var checkedboxes = $( "input:checked" );
+        for (var i = 0; i < checkedboxes.length; i++) {
+        }  
+        console.log(checkedboxes);
+        var filteredSongs = _.filter(allSongsArray, function(song) {
+          if (song.artist === currentArtist || song.album === currentAlbum) {
+            return true;
+          } else {
+            return false;
+          }
+        })
+        console.log(filteredSongs);
+        require(['hbs!../templates/songs'], function(songTemplate) {
+          $("#songList").html(songTemplate({songs:filteredSongs}));
+      
+        });
+     
+    }); 
 
-    // populate.getSongsOutput(pageDsp);
 
-    // $("#morebtn").click(function() {
-    //   counter ++;
-    //   //console.log(counter);
-  
-    //   getMore.getSongsOutput(pageDsp);
+  });
 
-    // });
+
     $("#addSong").click(function() {
       console.log("add songclicked");
       var artist = $("#newArtist").val();
@@ -126,70 +116,8 @@ requirejs(
       });
     });
 
-    $('#filterbtn').click(function() {
-      console.log("filter clicked");
 
-      var currentArtist = $('#artistdrpdwn').val(); 
-      console.log(currentArtist);
-      var currentAlbum = $('#albumdrpdwn').val(); 
-      console.log(currentAlbum);
-      var checkedboxes = $( "input:checked" );
-      for (var i = 0; i < checkedboxes.length; i++) {
-      }  
-      console.log(checkedboxes); 
-
-      // $('.filterSong').hide();
-      // var scoresRef = new Firebase("https://luminous-fire-170.firebaseio.com/");
-      // scoresRef.orderByValue().on("value", function(snapshot) {
-      //   snapshot.forEach(function(data) {
-      //   console.log("The " + data.key() + " dinosaur's score is " + data.val());
-      //   });
-      // });
-
-     
-
-
-      //var newArt = $('.media :contains(' + currentArtist + ')').parent().show();
- 
-      //$('.media :contains(' + albumFilter + ')').parent().show();
-
-
-    
-      //var ref = new Firebase("https://luminous-fire-170.firebaseio.com/songs.json");
-// Attach an asynchronous callback to read the data at our posts reference
-        //myFirebaseRef.on("value", function(snapshot) {
-        //   console.log(snapshot.val());
-
-        // }, function (errorObject) {
-        //   console.log("The read failed: " + errorObject.code);
-        // });
-
-
-
-
-
-      //var newArtist = $.get("https://luminous-fire-170.firebaseio.com/songs.json", function (data) {
-        //for(artist in newArtist) {
-        // var newA = Object.keys(newArtist).reduce(function(previous, current) {
-        //   previous[current] = newArtist[current];
-        //   return newA;
-        // }, {});
-
-        // console.log(newA);
-            
-      
-          //console.log(newArtist);
-          // $newArtist.map(function() {
-          //   console.log(this.currentArtist);
-          // })
-
-
-
-      })                           
-    //});
-
-});    
-
+  });
 
 
   
